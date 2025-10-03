fbab3891205e1f7141cab1c49227c675d32435dc
ArielK <ariel20030601@gmail.com>
2025-10-03 10:13:48 -0400

    Fixed web page
    Web page now loads (still needs to integrate Google Maps API to web)

--- SOEN343_BikeRentalService_ThinkVision2/components/Map.tsx
+++
@@ -1,7 +0,0 @@
-import { Platform } from 'react-native';
-import MapWeb from './MapWeb';
-import MapNative from './MapNative';
-
-const MapScreen = Platform.OS === 'web' ? MapWeb : MapNative;
-
-export default MapScreen;

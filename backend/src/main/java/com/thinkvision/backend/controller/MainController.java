048f8ab10e1f895e130ba771104ea23effa3940f
memoud0 <30944992+memoud0@users.noreply.github.com>
2025-10-05 03:42:05 -0400

    Add user entity, repository, and MySQL integration
    Introduces User entity, UserRepository, and MainController for basic user CRUD operations. Adds MySQL, JPA, and Flyway dependencies to pom.xml and configures database connection in application.yml. Moves HomeController to a new package for better organization.

---
+++ backend/src/main/java/com/thinkvision/backend/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.thinkvision.backend.controller;
+
+import com.thinkvision.backend.entity.User;
+import com.thinkvision.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewUser(@RequestParam String name,
+                                           @RequestParam String email) {
+
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+}

db2c9c59a39cc05807ff6e2ad7627190b73ca6ca
Robert <robertc1612@gmail.com>
2025-10-24 00:55:15 -0400

    Addition of sign out button
    Sign out button on profile tab that allows user to log out, user then back at home page and is able to log back in

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,15 +1,14 @@
 import { Stack } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
+import { AuthProvider } from '../contexts/AuthContext';
 
 export default function RootLayout() {
   return (
-    <>
+    <AuthProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="other_pages/login" options={{ headerShown: false }} />
-        <Stack.Screen name="other_pages/signup" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="dark" />
-    </>
+    </AuthProvider>
   );
 }
